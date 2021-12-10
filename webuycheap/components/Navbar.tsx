1f88ed925e8cd73a0d252d65a149342d4a8987de
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2021-12-10 15:21:09 +0100

    Added unstyled navigation bar

---
+++ webuycheap/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = (): JSX.Element => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/products">products</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ webuycheap/pages/products/index.tsx
@@ -0,0 +1,9 @@
+const Products = (): JSX.Element => {
+  return (
+    <>
+      <h1>Products</h1>
+    </>
+  )
+}
+
+export default Products;
